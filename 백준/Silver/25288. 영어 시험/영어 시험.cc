9da9017e514f4534d49f64e7d0ead57a18c11a3b
Tofu <141290630+iygav1238@users.noreply.github.com>
2025-04-27 23:18:59 +0900

    [Silver IV] Title: 영어 시험, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/25288. 영어 시험/영어 시험.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+const int MOD = 998244353;
+const int INF = 1000000000000000002LL;
+
+void solve() {
+    int n; cin >> n;
+    string s; cin >> s;
+    for (int i = 0; i<n; i++) cout << s;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    solve();    
+    return 0;
+}
