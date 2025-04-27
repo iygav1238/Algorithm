85ae95c68fb918d39f24edb90ada1c70c3e2d034
Tofu <141290630+iygav1238@users.noreply.github.com>
2025-04-27 23:16:00 +0900

    [Silver IV] Title: 동가수열 구하기, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/25184. 동가수열 구하기/동가수열 구하기.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+const int MOD = 998244353;
+const int INF = 1000000000000000002LL;
+
+void solve() {
+    int n; cin >> n;
+    vector<int> res(n, 0);
+    int l = 1, r = n/2+1;
+    for (int i = 0; i<n; i++) {
+        if (i & 1) res[i] = l++;
+        else res[i] = r++;
+    }
+    for (auto x : res) cout << x << " ";
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    solve();    
+    return 0;
+}
