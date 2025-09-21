151c8facb64de70049fd290fdbaf9e36122e0a69
Tofu <141290630+iygav1238@users.noreply.github.com>
2025-09-21 23:09:12 +0900

    [Gold III] Title: 약수 지우기 게임 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12107. 약수 지우기 게임 1/약수 지우기 게임 1.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+const int MOD = 998244353;
+const int INF = 1000000000000000002LL;
+
+void solve() {
+    int n; cin >> n;
+    cout << "AB"[n==1] << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    solve();    
+    return 0;
+}
