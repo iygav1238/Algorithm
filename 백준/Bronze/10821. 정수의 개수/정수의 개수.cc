e993b40ab58c3f5d5f2d89d73cdeaa35d9dd188c
Tofu <141290630+iygav1238@users.noreply.github.com>
2025-10-16 06:38:47 +0900

    [Bronze II] Title: 정수의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10821. 정수의 개수/정수의 개수.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+const int MOD = 998244353;
+const int INF = 1000000000000000002LL;
+
+void solve() {
+    string s; cin >> s;
+    int res = 1;
+    for (auto x : s) res += (x == ',');
+    cout << res << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    solve();    
+    return 0;
+}
