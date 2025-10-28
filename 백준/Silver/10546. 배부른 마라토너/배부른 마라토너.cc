7663e96b6fb89ee7d1cddf4b9f72ca9e8895f659
Tofu <141290630+iygav1238@users.noreply.github.com>
2025-10-28 23:29:29 +0900

    [Silver IV] Title: 배부른 마라토너, Time: 176 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Silver/10546. 배부른 마라토너/배부른 마라토너.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+#define FOR(i, a, b, c) for (int i = a; (c > 0 ? i<=b : i>=b); i+=c)
+
+#define heap priority_queue
+#define pii pair<int, int>
+#define tiii tuple<int, int, int>
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define vpii vector<pair<int, int>>
+
+const int MOD = 998244353;
+const int INF = 1e18+2;
+
+void solve() {
+    int n; cin >> n;
+    map<string, int> a, b;
+    FOR(i, 1, n, 1) {
+        string s; cin >> s;
+        a[s]++;
+    }
+    FOR(i, 1, n-1, 1) {
+        string s; cin >> s;
+        b[s]++;
+    }
+
+    for (auto [x, c] : a) {
+        int c2 = b[x];
+        if (c != c2) cout << x << endl;
+    }
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    solve();    
+    return 0;
+}
