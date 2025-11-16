bd1369a41fed4b4c2d4b8ee74ce40d433597116a
Tofu <141290630+iygav1238@users.noreply.github.com>
2025-11-16 13:43:44 +0900

    [Silver III] Title: 개미, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10158. 개미/개미.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+#define FOR(i, a, b, c) for (int i = a; (c > 0 ? i<=b : i>=b); i+=c)
+
+#define heap priority_queue
+#define pii pair<int, int>
+#define tiii tuple<int, int, int>
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define vpii vector<pair<int, int>>
+
+#define YES "YES"
+#define NO "NO"
+
+const int MOD = 998244353;
+const int INF = 1e18+2;
+
+void solve() {
+    int w, h, p, q, t;
+    cin >> w >> h >> p >> q >> t;
+    int x = w - abs(w - (p+t)%(2*w));
+    int y = h - abs(h - (q+t)%(2*h));
+    cout << x << " " << y << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    solve();    
+    return 0;
+}
